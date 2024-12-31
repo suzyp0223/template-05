d862fd418d54d3271a738fbdf212774553831f4a
suzy <suzyp0223@gmail.com>
2024-12-31 22:11:53 +0900

    05과제 진행중 미완성 제출

--- components/Nav.js
+++ components/Nav.js
@@ -1,1 +1,33 @@
-// do something!
+const categories = [
+  { id: 'all', name: '전체보기' },
+  { id: 'business', name: '비즈니스' },
+  { id: 'entertainment', name: '엔터테인먼트' },
+  { id: 'health', name: '건강' },
+  { id: 'science', name: '과학' },
+  { id: 'sports', name: '스포츠' },
+  { id: 'technology', name: '기술' },
+];
+
+const Nav = () => {
+  const nav = document.createElement('category-list');
+  nav.className = 'category-list';
+  const ul = document.createElement('ul');
+
+  categories.forEach(category => {
+    const li = document.createElement('li');
+    li.id = category.id;
+    li.className = 'category-item';
+    li.textContent = category.name;
+
+    li.addEventListener('click', () => {
+      console.log(`Category ${category.name} clicked`);
+    });
+
+    ul.appendChild(li);
+  });
+
+  nav.appendChild(ul);
+  return nav;
+};
+
+export default Nav;
--- components/NewsList.js
+++ components/NewsList.js
@@ -1,1 +1,60 @@
-// do something!
+const NewsList = () => {
+  const container = document.createElement('div');
+  container.className = 'news-list-container';
+
+  const renderNews = (newsItems) => {
+    container.innerHTML = '';
+    newsItems.forEach(item => {
+      const article = document.createElement('article');
+      article.className = 'news-list';
+
+      article.innerHTML =/*HTML */ `
+        <section class="news-item">
+          <div class="thumbnail">
+            <a href="${item.url}" target="_blank" rel="noopener noreferrer">
+              <img src="${item.img}" alt="thumbnail" />
+            </a>
+          </div>
+          <div class="contents">
+            <h2>
+              <a href="${item.url}" target="_blank" rel="noopener noreferrer">
+                ${item.title}
+              </a>
+            </h2>
+            <p>${item.description}</p>
+          </div>
+        </section>
+      `;
+      container.appendChild(article);
+    });
+  };
+
+  const fetchNews = async (category = 'all') => {
+    const pageSize = 5;
+    let page = 1;
+
+    const apiKEY = 'API_KEY';
+    // pageSize에 5를 지정하면 5개의 뉴스를 취득한다.
+    const apiURL = `https://newsapi.org/v2/top-headlines?country=kr&category=${category === 'all' ? '' : category}&page=${page}&pageSize=${pageSize}&apiKEY=${apiKEY}`;
+
+    try {
+      const response = await axios.get(apiURL);
+      const newsItems = response.data.articles.map(article => ({
+        title: article.title,
+        description: article.description,
+        image: article.urlToImage,
+        url: article.url,
+      }));
+
+      renderNews(newsItems);
+    } catch (error) {
+      console.error('Failed to fetch news:', error);
+    }
+  };
+
+  // 초기 데이터 로드
+  fetchNews();
+
+  return container;
+};
+export default NewsList;
